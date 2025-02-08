0b6589c2c4d9ee8e84412f347b2cadf9294b78bc
L I T O <76776795+Lito08@users.noreply.github.com>
2025-02-09 02:33:30 +0800

    Clean

--- FaceRecAttendance/attendance_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import take_attendance, attendance_records, recognize_face
-
-urlpatterns = [
-    path("take/", take_attendance, name="take_attendance"),
-    path("records/", attendance_records, name="attendance_records"),
-    path("recognize/", recognize_face, name="recognize_face"),
-]
--- FaceRecAttendance/materials_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaterialsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'materials_app'
